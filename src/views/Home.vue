06c444ef674ac19f2a99c687560a6b0fdfa01e2c
Nicolás Calvache <nicolas.calvache@alumnos.uneatlantico.es>
2021-04-24 13:10:17 +0200

    added axios test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  beforeMount() {
+    axios
+      .get("http://localhost:4000/location/infected")
+      .then((res) => {
+        console.log(res.data.records);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+};
 </script>
