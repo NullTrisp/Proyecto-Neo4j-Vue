e681953e82e39a60943cacb9b94dafa4f5774afd
Nicolás Calvache Jiménez <nicolas.calvache@alumnos.uneatlantico.es>
2021-04-27 19:43:40 +0200

    sigma working static

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="sigmacontainer"></div>
+  <div id="sigma-container"></div>
 </template>
 
 <script>
@@ -7,18 +7,65 @@ import sigma from "sigma";
 export default {
   name: "Graph",
 
-  props: ["graph"],
+  // props: ["graph"],
 
   data() {
     return {
       canvas: {},
+      graph: {
+        nodes: [
+          { id: "n0", label: "A node", x: 0, y: 0, size: 3, color: "#008cc2" },
+          {
+            id: "n1",
+            label: "Another node",
+            x: 3,
+            y: 1,
+            size: 2,
+            color: "#008cc2",
+          },
+          {
+            id: "n2",
+            label: "And a last one",
+            x: 1,
+            y: 3,
+            size: 1,
+            color: "#E57821",
+          },
+        ],
+        edges: [
+          {
+            id: "e0",
+            source: "n0",
+            target: "n1",
+            color: "#282c34",
+            type: "line",
+            size: 0.5,
+          },
+          {
+            id: "e1",
+            source: "n1",
+            target: "n2",
+            color: "#282c34",
+            type: "curve",
+            size: 1,
+          },
+          {
+            id: "e2",
+            source: "n2",
+            target: "n0",
+            color: "#FF0000",
+            type: "line",
+            size: 2,
+          },
+        ],
+      },
     };
   },
 
   mounted() {
     this.canvas = new sigma({
       renderer: {
-        container: document.getElementById("sigmacontainer"),
+        container: document.getElementById("sigma-container"),
         type: "canvas",
       },
       settings: {
