84ac159fd8b138c84ca28928532b7b3da24c1544
Nicolás Calvache Jiménez <nicolas.calvache@alumnos.uneatlantico.es>
2021-04-27 18:31:22 +0200

    added sigma

---
+++ src/components/Graph.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="sigmacontainer"></div>
+</template>
+
+<script>
+import sigma from "sigma";
+export default {
+  name: "Graph",
+
+  props: ["graph"],
+
+  data() {
+    return {
+      canvas: {},
+    };
+  },
+
+  mounted() {
+    this.canvas = new sigma({
+      renderer: {
+        container: document.getElementById("sigmacontainer"),
+        type: "canvas",
+      },
+      settings: {
+        minEdgeSize: 0.1,
+        maxEdgeSize: 2,
+        minNodeSize: 1,
+        maxNodeSize: 8,
+      },
+    });
+
+    this.canvas.graph.read(this.graph);
+    this.canvas.refresh();
+  },
+};
+</script>
+
+<style scoped>
+#sigma-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,90 @@
 <template>
-  <hello-world />
+  <Graph :graph="graphData" />
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import Graph from "../components/Graph";
+import axios from "axios";
 
-  export default {
-    name: 'Home',
+export default {
+  name: "Home",
 
-    components: {
-      HelloWorld,
-    },
-  }
+  components: {
+    Graph,
+  },
+
+  data() {
+    return {
+      graphData: {
+        nodes: [
+          { id: "n0", label: "A node", x: 0, y: 0, size: 3, color: "#008cc2" },
+          {
+            id: "n1",
+            label: "Another node",
+            x: 3,
+            y: 1,
+            size: 2,
+            color: "#008cc2",
+          },
+          {
+            id: "n2",
+            label: "And a last one",
+            x: 1,
+            y: 3,
+            size: 1,
+            color: "#E57821",
+          },
+        ],
+        edges: [
+          {
+            id: "e0",
+            source: "n0",
+            target: "n1",
+            color: "#282c34",
+            type: "line",
+            size: 0.5,
+          },
+          {
+            id: "e1",
+            source: "n1",
+            target: "n2",
+            color: "#282c34",
+            type: "curve",
+            size: 1,
+          },
+          {
+            id: "e2",
+            source: "n2",
+            target: "n0",
+            color: "#FF0000",
+            type: "line",
+            size: 2,
+          },
+        ],
+      },
+    };
+  },
+
+  beforeCreate() {
+    axios
+      .get("http://localhost:4000/person/infected")
+      .then((res) => {
+        let count = 0;
+        this.graphData.nodes = res.data.records.map((el) => {
+          console.log(el._fields[0].properties);
+          return {
+            id: count++,
+            label: el._fields[0].properties.name,
+            size: 3,
+            color: "#E57821",
+            x: 0,
+            y: 0,
+          };
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+};
 </script>
